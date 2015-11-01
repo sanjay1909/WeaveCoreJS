8740a209d0c76535be40bd01722b73f6c2632ef3
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-11-01 01:58:22 -0500

    WeavePromise ported, AsyncToken and Asynresponder ported from As3

--- weavecore.js
+++ weavecore.js
@@ -5494,6 +5494,7 @@ Object.defineProperty(WeaveAPI, 'TASK_PRIORITY_LOW', {
 
 //WeaveAPI.SessionManager = new weavecore.SessionManager();
 //WeaveAPI.globalHashMap = new weavecore.LinkableHashMap();
+
 /**
  * @module weavecore
  */
@@ -8521,6 +8522,7 @@ if (typeof window === 'undefined') {
     }
 
 }());
+
 if (typeof window === 'undefined') {
     this.weavecore = this.weavecore || {};
 } else {
@@ -9741,6 +9743,286 @@ if (typeof window === 'undefined') {
     weavecore.SortOn = SortOn;
 }())
 
+if (typeof window === 'undefined') {
+    this.weavecore = this.weavecore || {};
+} else {
+    window.weavecore = window.weavecore || {};
+}
+
+
+(function () {
+    /**
+     * Use this when you need a Promise chain to depend on ILinkableObjects and resolve multiple times.
+     *
+     * Adds support for <code>depend(...linkableObjects)</code>
+     * @param relevantContext This parameter may be null.  If the relevantContext object is disposed, the promise will be disabled.
+     * @param resolver A function like function(resolve:Function, reject:Function):void which carries out the promise
+     */
+    function WeavePromise(relevantContext, resolver) {
+        resolver = (resolver === undefined) ? null : resolver;
+
+
+        this._rootPromise;
+        this.relevantContext;
+        this._result;
+        this._error;
+        Object.defineProperty(this, '_handlers', {
+            value: []
+        });
+        Object.defineProperty(this, '_dependencies', {
+            value: []
+        });
+
+
+
+        if (relevantContext instanceof WeavePromise) {
+            // this is a child promise
+            this._rootPromise = relevantContext._rootPromise;
+            this.relevantContext = relevantContext = this._rootPromise.relevantContext;
+        } else {
+            // this is a new root promise
+            this._rootPromise = this;
+            this.relevantContext = relevantContext;
+
+            // if resolver is not specified, immediately set the result of the root promise equal to the relevantContext
+            if (resolver === null)
+                this.setResult(this.relevantContext);
+        }
+
+        if (relevantContext)
+            WeaveAPI.SessionManager.registerDisposableChild(relevantContext, this);
+
+        if (resolver != null) {
+            setBusy.call(this, true);
+            resolver(this.setResult, this.setError);
+        }
+    }
+
+
+    WeavePromise._noop = function (value) {
+        return value;
+    }
+
+    var p = WeavePromise.prototype;
+
+    p.setResult = function (result) {
+        if (WeaveAPI.SessionManager.objectWasDisposed(this.relevantContext)) {
+            setBusy.call(this, false);
+            return;
+        }
+
+        this._result = undefined;
+        this._error = undefined;
+
+        if (result instanceof weavecore.AsyncToken) {
+            result.addResponder(new weavecore.AsyncResponder(
+                function (result, token) {
+                    this.setResult(result);
+                }.bind(this),
+                function (fault, token) {
+                    this.setError(fault);
+                }.bind(this)
+            ));
+        } else if (result instanceof WeavePromise) {
+            result.then(this.setResult.bind(this), this.setError.bind(this));
+        } else {
+            this._result = result;
+            callHandlers.call(this);
+        }
+    }
+
+    p.getResult = function () {
+        return this._result;
+    }
+
+    p.setError = function (error) {
+        if (WeaveAPI.SessionManager.objectWasDisposed(this.relevantContext)) {
+            setBusy.call(this, false);
+            return;
+        }
+
+        this._result = undefined;
+        this._error = error;
+
+        callHandlers.call(this);
+    }
+
+    p.getError = function () {
+        return this._error;
+    }
+
+    function setBusy(busy) {
+        if (busy) {
+            var context = (this.relevantContext && (this.relevantContext instanceof weavecore.ILinkableObject || this.relevantContext.sessionable)) ? this.relevantContext : null;
+            WeaveAPI.ProgressIndicator.addTask(this._rootPromise, context);
+        } else {
+            WeaveAPI.ProgressIndicator.removeTask(this._rootPromise);
+        }
+    }
+
+    function callHandlers(newHandlersOnly) {
+        newHandlersOnly = (newHandlersOnly === undefined) ? false : newHandlersOnly;
+        if (this._dependencies.some(WeavePromise._dependencyIsBusy)) {
+            if (this._handlers.length)
+                setBusy.call(this, true);
+            return;
+        }
+
+        // if there are no more handlers, remove the task
+        if (this._handlers.length === 0)
+            setBusy.call(this, false);
+
+        if (WeaveAPI.SessionManager.objectWasDisposed(this.relevantContext)) {
+            setBusy.call(this, false);
+            return;
+        }
+
+        for (var i = 0; i < this._handlers.length; i++) {
+            var handler = this._handlers[i];
+            if (newHandlersOnly && handler.wasCalled)
+                continue;
+            if (this._result !== undefined)
+                handler.onResult(this._result);
+            else if (this._error !== undefined)
+                handler.onError(this._error);
+        }
+    }
+
+    p.then = function (onFulfilled, onRejected) {
+        onFulfilled = (onFulfilled === undefined) ? null : onFulfilled;
+        onRejected = (onRejected === undefined) ? null : onRejected;
+        if (onFulfilled === null)
+            onFulfilled = WeavePromise._noop;
+        if (onRejected === null)
+            onRejected = WeavePromise._noop;
+
+        var next = new WeavePromise(this);
+        next._result = undefined;
+        this._handlers.push(new weavecore.Handler(onFulfilled, onRejected, next));
+
+        if (this._result !== undefined || this._error !== undefined) {
+            // callLater will not call the function if the context was disposed
+            WeaveAPI.StageUtils.callLater(this.relevantContext, callHandlers, [true]);
+            setBusy.call(this, true);
+        }
+
+        return next;
+    }
+
+    p.depend = function () {
+        var linkableObjects = Array.prototype.slice.call(arguments);
+        if (linkableObjects.length) {
+            setBusy.call(this, true);
+        }
+        linkableObjects.forEach(function (dependency) {
+            WeaveAPI.SessionManager.getCallbackCollection(dependency).addGroupedCallback(this.relevantContext, callHandlers, true);
+        });
+        return this;
+    }
+
+    WeavePromise._dependencyIsBusy = function (dependency, i, a) {
+        return WeaveAPI.SessionManager.linkableObjectIsBusy(dependency);
+    }
+
+    p.getAsyncToken = function () {
+        var asyncToken = new weavecore.AsyncToken();
+        this.then(
+            function (result) {
+                asyncToken.applyResult(result, asyncToken);
+            },
+            function (error) {
+                var fault = "Error: Broken promise\n" + "Content: " + error;
+                asyncToken.applyFault(fault, asyncToken);
+            }
+        );
+        return asyncToken;
+    }
+
+    p.dispose = function () {
+        this._handlers.length = 0;
+        setBusy.call(this, false);
+    }
+
+    weavecore.WeavePromise = WeavePromise;
+
+}());
+
+(function () {
+    function Handler(onFulfilled, onRejected, next) {
+        this.next = next;
+        this.onFulfilled = onFulfilled;
+        this.onRejected = onRejected;
+        /**
+         * Used as a flag to indicate whether or not this handler has been called
+         */
+        this.wasCalled = false;
+    }
+
+    var p = Handler.prototype;
+
+    p.onResult = function (result) {
+        this.wasCalled = true;
+        try {
+            next.setResult(this.onFulfilled(result));
+        } catch (e) {
+            this.onError(e);
+        }
+    }
+
+    p.onError = function (error) {
+        this.wasCalled = true;
+        try {
+            next.setError(this.onRejected(error));
+        } catch (e) {
+            next.setError(e);
+        }
+    }
+
+    weavecore.Handler = Handler;
+}());
+
+
+(function () {
+    function AsyncToken() {
+        Object.defineProperty(this, 'responders', {
+            value: []
+        });
+    }
+
+    var p = AsyncToken.prototype;
+
+    p.addResponder = function (responder) {
+        this.responders.push(responder);
+    }
+
+    p.applyResult = function (result) {
+        for (var i = 0; i < this.responders.length; i++) {
+            this.responders[i].result(result, this.responders[i].token);
+        }
+    }
+
+    p.applyFault = function (fault) {
+        for (var i = 0; i < this.responders.length; i++) {
+            this.responders[i].fault(fault, this.responders[i].token);
+        }
+    }
+
+    weavecore.AsyncToken = AsyncToken;
+
+}());
+
+
+(function () {
+    function AsyncResponder(result, fault, token) {
+
+        this.result = result;
+        this.fault = fault;
+        this.token = token;
+
+    }
+    weavecore.AsyncResponder = AsyncResponder;
+
+}());
 /**
  * @module WeaveAPI
  */
@@ -11767,6 +12049,7 @@ if (typeof window === 'undefined') {
         window.WeaveAPI.globalHashMap = new LinkableHashMap();
     }
 }());
+
 /**
  * @module weavecore
  */
