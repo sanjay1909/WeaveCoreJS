90848a9c19c175648785176c61295c8d67eb6834
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-11-06 15:01:18 -0500

    Class Extends Support Added

--- weavecore.js
+++ weavecore.js
@@ -18,7 +18,13 @@ if (typeof window === 'undefined') {
 
     Object.defineProperty(ClassUtils, 'classNameLookUp', {
         value: {} //class -> className
+    });
+
+    Object.defineProperty(ClassUtils, 'classExtendsMap', {
+        value: {}
     })
+
+
 
     ClassUtils.registerClass = function (className, klass) {
         if (!ClassUtils.classLookUp[className]) {
@@ -47,11 +53,62 @@ if (typeof window === 'undefined') {
             return false;
     }
 
+    ClassUtils.getClassName = function (classDefn) {
+        return ClassUtils.classNameLookUp[classDefn];
+
+    }
+
+    /**
+     * @param classQName A qualified class name of a class in question.
+     * @param extendsQName A qualified class name that the class specified by classQName may extend.
+     * @return true if clasQName extends extendsQName, or if the two QNames are equal.
+     */
+    ClassUtils.classExtends = function (classQName, extendsQName) {
+        if (classQName === extendsQName)
+            return true;
+        try {
+            if (!cacheClassInfo(classQName))
+                return false;
+            return classExtendsMap[classQName][extendsQName] !== undefined;
+        } catch (e) {
+            console.log(e.stack());
+        }
+        return false;
+    }
+
+
+
+    /**
+     * This function will populate the  classExtendsMap for the given qualified class name.
+     * @param classQName A qualified class name.
+     * @return true if the class info has been cached.
+     */
+    ClassUtils.cacheClassInfo = function (classQName) {
+        if (classExtendsMap[classQName] !== undefined)
+            return true; // already cached
+
+        var classDef = ClassUtils.getClassDefinition(classQName);
+        if (classDef === null || classDef === undefined)
+            return false;
+
+
+        var eMap = new Object();
+        var _extends = classDef.prototype;
+
+        while (_extends) {
+            if (ClassUtils.getClassName(_extends))
+                eMap[ClassUtils.getClassName(_extends)] = true;
+            _extends = _extends.prototype ? _extends.prototype : _extends.__proto__;
+        }
+        classExtendsMap[classQName] = eMap;
+
+        return true; // successfully cached
+    }
+
     weavecore.ClassUtils = ClassUtils;
     //WeaveAPI.ClassUtils = new ClassUtils();
 
 }());
-
 Array.CASEINSENSITIVE = 1;
 Array.DESCENDING = 2;
 Array.UNIQUESORT = 4;
@@ -12844,7 +12901,7 @@ if (typeof window === 'undefined') {
         if (typeRestriction === undefined) typeRestriction = null;
         // this is a constraint on the type of object that can be linked
         this._typeRestrictionClassName;
-        this._typeRestriction = typeRestriction;
+        //this._typeRestriction = typeRestriction;
         // when this is true, the linked object cannot be changed
         this._locked = false;
 
@@ -13117,9 +13174,10 @@ if (typeof window === 'undefined') {
 
         this.targetPath = null;
 
-        if ((classDef.prototype instanceof weavecore.ILinkableObject || classDef.SESSIONABLE) && (this._typeRestriction === null || this._typeRestriction === undefined || classDef === this._typeRestriction)) {
-            var classDef = weavecore.ClassUtils.hasClassDefinition(className) ? weavecore.ClassUtils.getClassDefinition(className) : null;
-            classDef = classDef ? classDef : eval(className);
+        var classDef = weavecore.ClassUtils.hasClassDefinition(className) ? weavecore.ClassUtils.getClassDefinition(className) : null;
+        classDef = classDef ? classDef : eval(className);
+        if ((classDef.prototype instanceof weavecore.ILinkableObject || classDef.SESSIONABLE) && (this._typeRestriction === null || this._typeRestriction === undefined || weavecore.ClassUtils.classExtends(className, this._typeRestrictionClassName))) {
+
             var obj = this.target;
             if (!obj || obj.constructor !== classDef || !(obj instanceof classDef))
                 weavecore.LinkableWatcher.prototype.target = new classDef();
